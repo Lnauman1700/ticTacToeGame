93b1d85052ac98120b1602422944bc1cd8d2c708
Spencer Nauman <snauman817@gmail.com>
2018-11-12 13:58:22 -0500

    xTurn mapped to return value of getFirstPlayer

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-let isXturn;
-
 //generate random starting player
 const getFirstPlayer = () => {
   let rando = Math.floor(Math.random() * 2) + 1;
   if(rando == 1) {
-    isXturn = true;
+    return true;
   }
   else {
-    isXturn = false;
+    return false;
   }
 };
 
+let isXturn = getFirstPlayer();
 
 //function that places the x or o down in a grid.
 //based on the boolean value of who's turn it is.
@@ -45,7 +44,7 @@ let c1 = document.querySelector(".rowC > .column1");
 let c2 = document.querySelector(".rowC > .column2");
 let c3 = document.querySelector(".rowC > .column3");
 
-let a1Event = a1.addEventListener("click", placeShape(a1));
+let a1Event = a1.addEventListener("click", placeShape);
 let a2Event = a2.addEventListener("click", placeShape(a2));
 let a3Event = a3.addEventListener("click", placeShape(a3));
 let b1Event = b1.addEventListener("click", placeShape(b1));
