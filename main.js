343af2829bd43bfc8e37dd3406268559ab2a28c6
Lucas Nauman <Lnauman1700@gmail.com>
2018-11-12 13:23:30 -0500

    referenced the script in index.html and created the elements in main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-/*let a1 = querySelector("#a1");
-let a2 = querySelector("#a2");
-let a3 = querySelector("#a3");
-let b1 = querySelector("#b1");
-let b2 = querySelector("#b2");
-let b3 = querySelector("#b3");
-let c1 = querySelector("#c1");
-let c2 = querySelector("#c2");
-let c3 = querySelector("#c3");*/
+let a1 = document.querySelector(".rowA > .column1");
+let a2 = document.querySelector(".rowA > .column2");
+let a3 = document.querySelector(".rowA > .column3");
+let b1 = document.querySelector(".rowB > .column1");
+let b2 = document.querySelector(".rowB > .column2");
+let b3 = document.querySelector(".rowB > .column3");
+let c1 = document.querySelector(".rowC > .column1");
+let c2 = document.querySelector(".rowC > .column2");
+let c3 = document.querySelector(".rowC > .column3");
 let isXturn;
 
 //generate random starting player
@@ -20,16 +20,21 @@ const getFirstPlayer = () => {
   }
 };
 
-let slots = querySelectorAll(".slot");
-
-for(let i = 0; i < slots.length; i++) {
-  slots[i].addEventListener("click", placeShape(slots[i]));
-}
 
 //function that places the x or o down in a grid.
 //based on the boolean value of who's turn it is.
 const placeShape = (grid) => {
+  if(isXturn) {
+    let x = document.createTextNode("X");
+    grid.appendChild(x);
+    grid.removeEventListener();
+  }
+  else {
+    let o = document.createTextNode("O");
+    grid.appendChild(o);
+  }
   //before we flip the boolean, call checkEnd();
+  checkEnd();
 };
 
 //function that checks whether someone won or lost
