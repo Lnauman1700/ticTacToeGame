e51649e1363591ff3ebfb92065dba526b5b54bf1
Spencer Nauman <snauman817@gmail.com>
2018-11-12 12:42:38 -0500

    basic script layout

---
+++ main.js
@@ -0,0 +1,16 @@
+//function that places the x or o down in a grid.
+//based on the boolean value of who's turn it is.
+const placeShape = (turn, grid) => {
+  //before we flip the boolean, call checkEnd();
+};
+
+//function that checks whether someone won or lost
+//determines the end of the game
+const checkEnd = () => {
+
+};
+
+//generate random starting player
+const getFirstPlayer = () => {
+
+};
